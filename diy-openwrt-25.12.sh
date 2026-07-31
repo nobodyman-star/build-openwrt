d55345770f5ef345f9de441d4f09c9ce3ac713fe
nobodyman-star <290378374@qq.com>
2026-08-01 07:34:42 +0800

    Update diy-openwrt-25.12.sh

--- diy-openwrt-25.12.sh
+++ diy-openwrt-25.12.sh
@@ -246,14 +246,14 @@ clone_all https://github.com/OpenListTeam/OpenList
 clone_all https://github.com/sbwml/luci-app-mosdns
 git_clone https://github.com/sbwml/packages_lang_golang golang
 clone_all https://github.com/destan19/OpenAppFilter
-# clone_all https://github.com/xuanranran/luci-app-onliner
+clone_all https://github.com/xuanranran/luci-app-onliner
 clone_all https://github.com/linkease/istore-ui
 clone_all https://github.com/linkease/istore luci
 clone_all https://github.com/chenmozhijin/luci-app-socat
 # clone_all https://github.com/asvow/luci-app-tailscale
 clone_all https://github.com/brvphoenix/luci-app-wrtbwmon
 clone_all https://github.com/brvphoenix/wrtbwmon
-clone_dir https://github.com/kiddin9/kwrt-packages luci-app-onliner luci-app-tailscale-community luci-app-wizard openwrt-bandix luci-app-bandix luci-theme-edge
+# clone_dir https://github.com/kiddin9/kwrt-packages luci-app-onliner luci-app-tailscale-community luci-app-wizard openwrt-bandix luci-app-bandix luci-theme-edge
 # 科学上网插件
 # clone_all https://github.com/fw876/helloworld
 # clone_all https://github.com/xiaorouji/openwrt-passwall-packages
