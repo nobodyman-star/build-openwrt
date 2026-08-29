43b10ebbba6e9b1d32b81b52dfec47f322dc54d7
nobodyman-star <290378374@qq.com>
2026-08-29 19:37:10 +0800

    Update diy-openwrt-25.12.sh

--- diy-openwrt-25.12.sh
+++ diy-openwrt-25.12.sh
@@ -260,7 +260,6 @@ clone_all https://github.com/sirpdboy/luci-app-poweroffdevice
 clone_all https://github.com/sirpdboy/luci-app-lucky
 clone_all https://github.com/OpenListTeam/OpenList
 clone_all https://github.com/sbwml/luci-app-mosdns
-git_clone https://github.com/sbwml/packages_lang_golang golang
 clone_all https://github.com/destan19/OpenAppFilter
 clone_all https://github.com/xuanranran/luci-app-onliner
 clone_all https://github.com/linkease/istore-ui
