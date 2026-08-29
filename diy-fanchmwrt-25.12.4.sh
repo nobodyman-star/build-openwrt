392793b67e618f5e40116fb6c6798fffcc93b915
nobodyman-star <290378374@qq.com>
2026-08-29 08:22:40 +0800

    Update diy-fanchmwrt-25.12.4.sh

--- diy-fanchmwrt-25.12.4.sh
+++ diy-fanchmwrt-25.12.4.sh
@@ -272,7 +272,7 @@ clone_all https://github.com/nikkinikki-org/OpenWrt-nikki
 clone_all https://github.com/nikkinikki-org/OpenWrt-momo
 git_clone https://github.com/sirpdboy/luci-app-timecontrol
 # clone_dir https://github.com/QiuSimons/luci-app-daed daed luci-app-daed
-clone_all https://github.com/sirpdboy/luci-app-netspeedtest
+# clone_all https://github.com/sirpdboy/luci-app-netspeedtest
 clone_all https://github.com/sirpdboy/luci-app-poweroffdevice
 clone_all https://github.com/sirpdboy/luci-app-lucky
 clone_all https://github.com/OpenListTeam/OpenList
