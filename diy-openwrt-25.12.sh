9dec8527ce22827979e79ff6e06fb51a928c6d31
nobodyman-star <290378374@qq.com>
2026-09-04 00:33:32 +0800

    Update diy-openwrt-25.12.sh

--- diy-openwrt-25.12.sh
+++ diy-openwrt-25.12.sh
@@ -306,12 +306,13 @@ fi
 sed -i 's#^src-git\s\+luci\s\+.*coolsnowwolf.*#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12#' feeds.conf.default
 
 # 修改显示信息
-BUILD_TIME=$(date +"%Y-%m-%d")
-sed -i "s|^DISTRIB_DESCRIPTION=.*|DISTRIB_DESCRIPTION='ImmortalWrt25.12 compiled by T on ${BUILD_TIME}'|" package/base-files/files/etc/openwrt_release
-JSFILE="feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js"
-# sed -i "s|boardinfo.release.description|\"ImmortalWrt25.12 compiled by T on ${BUILD_TIME}\"|" "${JSFILE}"
-sed -i "s|(L.isObject(boardinfo.release) ? boardinfo.release.description + ' \/ ' : '') + (luciversion || '')|(L.isObject(boardinfo.release) ? boardinfo.release.description + ' \/ ' : '') + (luciversion || '') + ' | Compiled on ${BUILD_TIME} by T'|" "${JSFILE}"
-
+ 
+ BUILD_TIME=$(date +"%Y-%m-%d")
+ RELEASE_FILE="package/base-files/files/etc/openwrt_release"
+ sed -i "s#^DISTRIB_DESCRIPTION=.*#DISTRIB_DESCRIPTION='ImmortalWrt25.12 compiled by T on ${BUILD_TIME}'#" "${RELEASE_FILE}"
+ JSFILE="feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js"
+ 
+ sed -i "s#(L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || '')#(L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || '') + ' | Compiled on ${BUILD_TIME} by T'#" "${JSFILE}"
 
 
 # 修改默认IP
