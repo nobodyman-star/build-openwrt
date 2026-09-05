d2f0b66107e1f5b4261de56b2e5e184ec4254633
nobodyman-star <290378374@qq.com>
2026-09-06 00:05:06 +0800

    Update preset-adguard-core.sh

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -e
 mkdir -p "files/usr/bin"
-AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_${1}.tar.gz"
-wget -qO- "$AGH_CORE" | tar -xOz AdGuardHome/AdGuardHome > "files/usr/bin/AdGuardHome"
-chmod +x "files/usr/bin/AdGuardHome"
+if [ -z "$1" ];then
+    echo "usage: $0 amd64|arm64|armv7|386|mipsle"
+    exit 1
+fi
+ARCH="$1"
+AGH_URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_${ARCH}.tar.gz"
+echo "Download: ${AGH_URL}"
+wget -qL -O - "${AGH_URL}" | tar -xO AdGuardHome/AdGuardHome > files/usr/bin/AdGuardHome
+chmod +x files/usr/bin/AdGuardHome
+echo "Done -> files/usr/bin/AdGuardHome"
+
 # 关闭 CONFIG_PACKAGE_adguardhome=y，避免冲突，改成CONFIG_PACKAGE_adguardhome=n
