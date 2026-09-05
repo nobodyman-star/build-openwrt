464e0814f21286fe8a15828e79fbbac9d90fec95
nobodyman-star <290378374@qq.com>
2026-09-06 07:33:58 +0800

    Fix extraction command for AdGuardHome

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -2,7 +2,7 @@
 
 [ -d files/usr/bin ] || mkdir -p files/usr/bin
 AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_${1}.tar.gz"
-wget -qO- $AGH_CORE | tar xOz > files/usr/bin/AdGuardHome
+wget -qO- $AGH_CORE | tar xOz 'AdGuardHome' > files/usr/bin/AdGuardHome
 chmod +x files/usr/bin/AdGuardHome
 echo "AGH_CORE: $AGH_CORE" 
 # 关闭 CONFIG_PACKAGE_adguardhome=y，避免冲突，改成CONFIG_PACKAGE_adguardhome=n
