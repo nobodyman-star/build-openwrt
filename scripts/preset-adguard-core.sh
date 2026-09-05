8b68a446596b358731c035b98790eefec20ba408
nobodyman-star <290378374@qq.com>
2026-09-06 07:15:23 +0800

    Fix directory creation for AdGuardHome installation

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -d files/usr/bin/AdGuardHome ] || mkdir -p files/usr/bin/AdGuardHome
+[ -d files/usr/bin ] || mkdir -p files/usr/bin
 AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_${1}.tar.gz"
 wget -qO- $AGH_CORE | tar xOz > files/usr/bin/AdGuardHome
 chmod +x files/usr/bin/AdGuardHome
